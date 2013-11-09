ad901616a96523030b451c4fbe0ebe9afbed1352
Raymond Jacobson <raymondshujacobson8@gmail.com>
2013-11-09 16:11:51 -0500

    add course model, annotate

---
+++ app/models/course.rb
@@ -0,0 +1,12 @@
+# == Schema Information
+#
+# Table name: courses
+#
+#  id         :integer          not null, primary key
+#  name       :string(255)
+#  created_at :datetime
+#  updated_at :datetime
+#
+
+class Course < ActiveRecord::Base
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131109053437) do
+ActiveRecord::Schema.define(version: 20131109210927) do
+
+  create_table "courses", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
