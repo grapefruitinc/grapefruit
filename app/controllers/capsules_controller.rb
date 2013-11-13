081f367f53cde117921d79d27e128431f3f27654
Raymond Jacobson <raymondshujacobson8@gmail.com>
2013-11-13 17:18:52 -0500

    add CRUD for capsule
    Would have used the name "Module," but Rails prevents that...

--- app/controllers/capsules_controller.rb
+++ app/controllers/capsules_controller.rb
@@ -1,2 +1,24 @@
 class CapsulesController < ApplicationController
+	def new
+  	@capsule = Capsule.new
+  end
+
+  def show
+  	@capsule = Capsule.find(params[:id])
+  end
+
+  def create
+  	@capsule = Capsule.new(params[:capsule])
+  	if @capsule.save
+  		flash[:success] = "Capsule created!"
+  		redirect_to @capsule
+  	else
+  		render 'new'
+  end
+
+  def destroy
+  	@capsule = @capsule.find(params[:id])
+  	@capsule.destroy
+  	redirect_to :back
+  end
 end
