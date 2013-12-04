8c73d5202331070929ed3f3af2c0cea4d4da70ee
Graham Ramsey <g@graham.li>
2013-12-04 18:49:27 -0500

    view instructing/enrolled courses

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,5 +3,7 @@ class HomeController < ApplicationController
   layout "home"
 
   def dashboard
+    @instructed_courses = current_user.instructed_courses
+    @enrolled_courses = current_user.student_courses
   end
 end
