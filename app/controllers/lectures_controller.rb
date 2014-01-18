70414ba781817c2f769ae924d2d0022837dd0edd
Damian Mastylo <dmastylo@codequarry.net>
2014-01-18 14:35:34 -0600

    Passwords styling

--- app/controllers/lectures_controller.rb
+++ app/controllers/lectures_controller.rb
@@ -2,6 +2,7 @@ class LecturesController < ApplicationController
 
   before_filter :get_course
   before_filter :get_capsule
+  before_filter :get_lecture, only: [:show, :edit, :update, :destroy]
   before_filter :authenticate_user!
 
   layout "home"
@@ -10,28 +11,44 @@ class LecturesController < ApplicationController
   	@lecture = @capsule.lectures.new
   end
 
-  def show
-  	@lecture = @capsule.lectures.find(params[:id])
+  def create
+    @lecture = @capsule.lectures.new(lecture_params)
+    if @lecture.save
+      flash[:success] = "Lecture created!"
+      redirect_to [@course, @capsule, @lecture]
+    else
+      render 'new'
+    end
   end
 
-  def create
-  	@lecture = @capsule.lectures.new(lecture_params)
-  	if @lecture.save
-  		flash[:success] = "Lecture created!"
-  		redirect_to [@course, @capsule, @lecture]
-  	else
-  		render 'new'
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @lecture.update_attributes(lecture_params)
+      flash[:success] = "Lecture updated!"
+      redirect_to [@course, @capsule, @lecture]
+    else
+      render "edit"
     end
   end
 
   def destroy
-  	@lecture = @lecture.find(params[:id])
   	@lecture.destroy
   	redirect_to :back
   end
 
 private
+
+  def get_lecture
+    @lecture = @capsule.lectures.find(params[:lecture_id] || params[:id])
+  end
+
   def lecture_params
     params.require(:lecture).permit(:name, :lecture_number, :mediasite_url)
   end
+
 end
