ca6c19c5a8e439b96349cd2d339a57c3bb623b47
Damian Mastylo <dmastylo@codequarry.net>
2014-02-01 22:16:16 -0500

    Changed to requests specs, made specs match new expect() syntax

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,11 @@ class ApplicationController < ActionController::Base
 
   before_filter :configure_permitted_parameters, if: :devise_controller?
 
-  protected
+  rescue_from CanCan::AccessDenied do |exception|
+    redirect_to root_url, alert: exception.message
+  end
+
+protected
  
   def configure_permitted_parameters
 
