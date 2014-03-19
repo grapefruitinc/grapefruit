e4b59ed587a31a9263da06fff60a12a95db69fbf
Damian Mastylo <dmastylo@gmail.com>
2014-03-19 13:19:21 -0400

    Schema

--- db/schema.rb
+++ db/schema.rb
@@ -107,8 +107,8 @@ ActiveRecord::Schema.define(version: 20140303014426) do
     t.string   "name"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
   create_table "video_texts", force: true do |t|
     t.text     "content"
