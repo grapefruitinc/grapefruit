59ecc0858ce4c16ec09e0b8df85d91259e8a187d
Graham Ramsey <g@graham.li>
2014-04-16 17:57:44 -0400

    cya for now turbolinks
    breaking too much stuff currently. we’ll go through later and fine-tune
    it so it works better

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,10 +11,8 @@
 // about supported directives.
 //
 //= require jquery
-//= require jquery.turbolinks
 //= require jquery_ujs
 //= require foundation
-//= require turbolinks
 //= require_tree .
 
 $("document").ready(function(){
