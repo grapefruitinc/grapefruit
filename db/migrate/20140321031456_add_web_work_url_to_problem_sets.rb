011826b35d47f0adead007db568d4966a78b7c89
austingulati <contact@austingulati.com>
2014-04-22 22:13:13 -0400

    Restore some changes

--- db/migrate/20140321031456_add_web_work_url_to_problem_sets.rb
+++
@@ -1,5 +0,0 @@
-class AddWebWorkUrlToProblemSets < ActiveRecord::Migration
-  def change
-    add_column :problem_sets, :webwork_url, :text
-  end
-end
--- db/migrate/20140422164629_remove_webwork_url_from_problem_sets.rb
+++
@@ -1,5 +0,0 @@
-class RemoveWebworkUrlFromProblemSets < ActiveRecord::Migration
-  def change
-    remove_column :problem_sets, :webwork_url, :string
-  end
-end
