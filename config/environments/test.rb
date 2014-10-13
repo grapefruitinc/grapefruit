45eb07cefa38810dd88c63480de14e2bc252fe72
Damian Mastylo <dmastylo@gmail.com>
2014-10-13 14:19:41 -0400

    Add emailing to test environment so now all tests that should pass, pass

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -1,3 +1,5 @@
+require 'yaml'
+
 Grapefruit::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
@@ -33,4 +35,18 @@ Grapefruit::Application.configure do
 
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
+
+  # Don't care if the mailer can't send.
+  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.default_url_options = { :host => Settings.gmail.source_host } # :protocol => 'https'}
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+     :address => "smtp.gmail.com",
+     :port => 587,
+     :authentication => :login,
+     :enable_starttls_auto => true,
+     :user_name => Settings.gmail.address,
+     :password => Settings.gmail.password
+   }
 end
