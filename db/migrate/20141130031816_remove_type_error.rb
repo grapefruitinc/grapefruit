dc56b800b0a524956fe0482a64bfba85ed0faa7a
Graham Ramsey <g@graham.li>
2014-11-29 22:32:10 -0500

    streamline student view and fix error

---
+++ db/migrate/20141130031816_remove_type_error.rb
@@ -0,0 +1,5 @@
+class RemoveTypeError < ActiveRecord::Migration
+  def change
+  	remove_column :course_users, :type
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141128231550) do
+ActiveRecord::Schema.define(version: 20141130031816) do
 
   create_table "active_admin_comments", force: true do |t|
     t.string   "namespace"
@@ -95,7 +95,6 @@ ActiveRecord::Schema.define(version: 20141128231550) do
     t.integer  "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "type",       default: 0
   end
 
   create_table "courses", force: true do |t|
