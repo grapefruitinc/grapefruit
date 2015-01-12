d99bf33458a7eff9789d1c0730da603814c79107
Graham Ramsey <g@graham.li>
2015-01-12 10:28:00 -0500

    build number

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -1,6 +1,7 @@
 GRAPEFRUIT_VERSION_MAJOR = 0
 GRAPEFRUIT_VERSION_MINOR = 5
 GRAPEFRUIT_VERSION_PATCH = 0
+GRAPEFRUIT_BUILD = 100
 GITHUB_URL  =   'https://github.com/grapefruitdotlink/grapefruit'
 SCHOOL_URL  =   'http://rpi.edu'
 SCHOOL_NAME =   'Rensselaer Polytechnic Institute'
