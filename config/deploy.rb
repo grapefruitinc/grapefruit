3a36439a9919336ec54ca94bc568226ffcafbaf3
Graham Ramsey <g@graham.li>
2015-02-18 23:28:40 -0500

    fixes for deployment

--- config/deploy.rb
+++ config/deploy.rb
@@ -25,7 +25,7 @@ set :stages, %w(stage1 stage2 production)
 # set :pty, true
 
 # Default value for :linked_files is []
-# set :linked_files, %w{config/database.yml config/settings.yml}
+set :linked_files, %w{config/database.yml config/settings.yml}
 
 # Default value for linked_dirs is []
 # set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
