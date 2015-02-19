60b53f36613f4b937fc4019cd6fe81ce0911470b
Graham Ramsey <g@graham.li>
2015-02-19 13:16:41 -0500

    move org info to settings from constants

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -4,7 +4,4 @@ GRAPEFRUIT_VERSION_PATCH = 5
 GRAPEFRUIT_BUILD = `git rev-parse --short HEAD`
 GITHUB_URL  =   'https://github.com/grapefruitdotlink/grapefruit'
 CHANGELOG_URL = 'http://grapefruit.link/tagged/changelog'
-ORG_URL  =   'http://rpi.edu'
-ORG_NAME =   'Rensselaer Polytechnic Institute'
-ORG_NAME_SHORT = 'RPI'
 DATE_FORMAT_DAY = '%B %-d, %Y'
