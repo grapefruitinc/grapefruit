c17b9d2ce5ef7dd561dce693014e74885b5ab905
Graham Ramsey <g@graham.li>
2015-10-20 17:12:45 -0400

    Update footer, constants, about, contact

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -2,6 +2,6 @@ GRAPEFRUIT_VERSION_MAJOR = 0
 GRAPEFRUIT_VERSION_MINOR = 5
 GRAPEFRUIT_VERSION_PATCH = 8
 GRAPEFRUIT_BUILD = `git rev-parse --short HEAD`
-GITHUB_URL  =   'https://github.com/grapefruitdotlink/grapefruit'
+GITHUB_URL  =   'https://github.com/grapefruitinc/grapefruit'
 CHANGELOG_URL = 'http://blog.grapefruit.link/tagged/changelog'
 DATE_FORMAT_DAY = '%B %-d, %Y'
