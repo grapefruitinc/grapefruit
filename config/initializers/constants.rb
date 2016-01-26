fae4563ea1d5f54a29dfae4a7a6570d9d04e860e
Graham Ramsey <g@graham.li>
2016-01-25 22:42:23 -0500

    Remove links to nonexistent Orchard/changelog

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -3,5 +3,4 @@ GRAPEFRUIT_VERSION_MINOR = 5
 GRAPEFRUIT_VERSION_PATCH = 9
 GRAPEFRUIT_BUILD = `git rev-parse --short HEAD`
 GITHUB_URL = 'https://github.com/grapefruitinc/grapefruit'
-CHANGELOG_URL = 'http://blog.grapefruit.link/tagged/changelog'
 DATE_FORMAT_DAY = '%B %-d, %Y'
