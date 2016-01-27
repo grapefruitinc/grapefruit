599add008d4ef1530259c021ec04060b77d9ced7
Graham Ramsey <g@graham.li>
2016-01-26 20:16:59 -0500

    Remove instructor label from courses
    Replace with a list of who is actually instructing the course

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160126234924) do
+ActiveRecord::Schema.define(version: 20160127010640) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string   "namespace",     limit: 255
@@ -115,7 +115,6 @@ ActiveRecord::Schema.define(version: 20160126234924) do
     t.string   "slug",                       limit: 255
     t.text     "problem_set_url",            limit: 65535
     t.integer  "school_account_id",          limit: 4
-    t.string   "instructor_label",           limit: 255
   end
 
   add_index "courses", ["school_account_id"], name: "index_courses_on_school_account_id", using: :btree
